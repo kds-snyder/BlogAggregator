a6382f892515a86fa45982fc3752b3c7a38bbf80
kds-snyder <kds_snyder@yahoo.com>
2015-11-30 13:43:10 -0800

    Removed references to author table in front end

--- BlogAggregator/BlogAggregator.Web/js/app/addblog.ctrl.js
+++ BlogAggregator/BlogAggregator.Web/js/app/addblog.ctrl.js
@@ -1,7 +1,6 @@
 //angular.module('app').controller('AddBlogController', function ($scope, Author, Blog) {
 angular.module('app').controller('AddBlogController', function ($scope) {
-
-    //$scope.author = new Author();
+   
     //$scope.blog = new Blog();
 
     $scope.addBlog() = function () {
