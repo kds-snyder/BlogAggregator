de3cd2daec8bc856247b47faadaa7ad0b5e2b14c
kds-snyder <kds_snyder@yahoo.com>
2015-11-30 13:33:57 -0800

    first commit

--- BlogAggregator/BlogAggregator.Web/js/app/authors.resource.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').factory('Author', function ($resource, apiUrl) {
-    
-    return $resource(apiUrl + 'api/authors/:id', { id: '@AuthorID' }, {   
-		update: {
-			method: 'PUT'
-		}
-	});
-});
