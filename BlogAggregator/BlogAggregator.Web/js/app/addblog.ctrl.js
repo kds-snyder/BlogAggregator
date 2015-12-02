21af5b586e0ce27962e9e545e830bf477492b0eb
kds-snyder <kds_snyder@yahoo.com>
2015-12-02 12:18:01 -0800

    Added Home button to top toolbar

--- BlogAggregator/BlogAggregator.Web/js/app/addblog.ctrl.js
+++ BlogAggregator/BlogAggregator.Web/js/app/addblog.ctrl.js
@@ -2,7 +2,44 @@ angular.module('app').controller('AddBlogController', function ($scope, Blog) {
   
     $scope.blog = new Blog();
 
+    /*
+    alert('After new: Blog ID: ' + $scope.blog.BlogID + ' CreatedDate: ' + $scope.blog.CreatedDate +
+            ' Approved: ' + $scope.blog.Approved + 
+            ' Description: ' + $scope.blog.Description + ' Link: ' + $scope.blog.Link +
+            ' Title: ' + $scope.blog.Title + ' Author Email: ' + $scope.blog.AuthorEmail
+            + ' Author Name: ' + $scope.blog.AuthorName);
+    */
+
     $scope.addBlog = function () {
-        
+
+        // TODO: remove when blog moderation is ready
+        $scope.blog.Approved = true;
+
+        // Save the new blog in the database
+        $scope.blog.$save(function () {
+            $mdToast.showSimple('Blog was added successfully');
+
+            // Clear the input form
+            /*
+            $scope.blog.BlogID = 0;
+            $scope.blog.Approved = false;
+            $scope.blog.authorEmail = '';
+            $scope.blog.authorName = '';
+            $scope.blog.Description = '';
+            $scope.blog.Link = '';
+            $scope.blog.Title = '';
+            */
+            $scope.blog = null;
+            $scope.addBlogForm.$setPristine();
+            $scope.addBlogForm.$setUntouched();
+
+        },
+        function (error) {
+            $mdToast.showSimple('Error adding blog. Please verify that '
+                        + $scope.blog.Link + ' is a link to a valid blog');
+
+            debugger;
+        });
+
     };
 });
