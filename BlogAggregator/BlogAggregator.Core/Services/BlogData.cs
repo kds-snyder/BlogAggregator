833af78990f863f9070f594de7cbe12769de404f
kds-snyder <kds_snyder@yahoo.com>
2015-12-02 19:39:37 -0800

    generated database

--- BlogAggregator/BlogAggregator.Core/Services/BlogData.cs
+++ BlogAggregator/BlogAggregator.Core/Services/BlogData.cs
@@ -20,37 +20,7 @@ namespace BlogAggregator.Core.Services
             _unitOfWork = unitOfWork;
         }
 
-        // Write the posts in the input posts to the database
-        // Return true if successful, otherwise return false
-        public bool SavePosts(List<Post> posts, int blogID)
-        {
-            bool result = true;
-
-            foreach (var post in posts)
-            {
-                _postRepository.Add(new Post
-                {
-                    BlogID = blogID,
-                    Content = post.Content,
-                    Description = post.Description,
-                    Link = post.Link,
-                    PublicationDate = post.PublicationDate,
-                    Title = post.Title
-                });
-            }
-
-            try
-            {
-                _unitOfWork.Commit();
-            }
-            catch (Exception e)
-            {
-
-                result = false;
-            }
-
-            return result;
-        }
+        
 
         public void Dispose()
         {
