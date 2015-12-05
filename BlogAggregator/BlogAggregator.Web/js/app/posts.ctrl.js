fb14a38dbe916895e0d967daeab356f5aff376ee
kds-snyder <kds_snyder@yahoo.com>
2015-12-05 00:50:54 -0800

    Add loading mask for posts

--- BlogAggregator/BlogAggregator.Web/js/app/posts.ctrl.js
+++ BlogAggregator/BlogAggregator.Web/js/app/posts.ctrl.js
@@ -1,6 +1,16 @@
-angular.module('app').controller('PostsController', function ($scope, Post) {
+angular.module('app').controller('PostsController', function ($scope, Post, PostService) {
 
-    $scope.posts = Post.query();
+    // Function to load posts, setting loading indicator while loading
+    $scope.load = function () {
+        $scope.loading = true;
+        $scope.posts = Post.query(function () {
+            $scope.loading = false;          
+        });
+    };
+
+   // $scope.posts = PostService.getAllPostsByDateDesc();
     
+    // After all definitions, load the posts
+    $scope.load();
     
 });
---
+++ BlogAggregator/BlogAggregator.Web/js/app/posts.service.js
@@ -0,0 +1,16 @@
+﻿angular.module('app').service('PostService', function ($http, apiUrl) {
+
+    return {
+        getAllPostsByDateDesc: function () {
+            var promise = $http.get(apiUrl + 'api/posts?$orderby=PublicationDate desc')
+                            .then(function (response) {
+                                return response.data;
+                            },
+                                function (error) {
+                                    return error.data;
+                                });
+            return promise;
+        }
+    };
+});
+
