06d8ca97592b60e98422ed12a934d8b3f00db3cf
kds-snyder <kds_snyder@yahoo.com>
2015-12-08 19:15:11 -0800

    Admin screen using tabs with ng-repeat but active tab heading does not show, and tab content has too much space before it

--- BlogAggregator/BlogAggregator.Web/js/administration/admin.ctrl.js
+++ BlogAggregator/BlogAggregator.Web/js/administration/admin.ctrl.js
@@ -1,5 +1,10 @@
 ﻿angular.module('app').controller('AdminController', function ($rootScope, $scope, $state) {
 
+    $scope.tabs = [
+        { heading: "Blog Moderation", state: "admin.blogs" },
+        { heading: "User Management", state: "admin.users" }
+    ];
+
     // Default administrative state is blog moderation
     $state.go('admin.blogs');
 });
