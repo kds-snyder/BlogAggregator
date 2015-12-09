e510c0207c18add746fe7e14066fc541961bbfea
kds-snyder <kds_snyder@yahoo.com>
2015-12-08 18:47:45 -0800

    Working on admin tabs

--- BlogAggregator/BlogAggregator.Web/js/administration/admin.ctrl.js
+++ BlogAggregator/BlogAggregator.Web/js/administration/admin.ctrl.js
@@ -1,3 +1,5 @@
 ﻿angular.module('app').controller('AdminController', function ($rootScope, $scope, $state) {
-    
+
+    // Default administrative state is blog moderation
+    $state.go('admin.blogs');
 });
