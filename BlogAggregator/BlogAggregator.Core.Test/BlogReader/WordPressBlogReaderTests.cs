70035fea964d465995ebf33b7aa9dc96edd018c0
kds-snyder <kds_snyder@yahoo.com>
2015-12-25 16:21:36 -0800

    Added tests for WordPressBlogReader

--- BlogAggregator/BlogAggregator.Core.Test/BlogReader/WordPressBlogReaderTests.cs
+++ BlogAggregator/BlogAggregator.Core.Test/BlogReader/WordPressBlogReaderTests.cs
@@ -5,36 +5,69 @@ using BlogAggregator.Core.BlogReader.WordPress;
 using BlogAggregator.Core.Domain;
 using System.Collections.Generic;
 using BlogAggregator.Core.Models;
+using BlogAggregator.Core.BlogReader;
 
 namespace BlogAggregator.Core.Test.BlogReader
 {
     [TestClass]
     public class WordPressBlogReaderTests
     {
+        // Real blog links
+        string[] _blogLinks =
+            {
+                "bracken.design",
+                "jeffwarddevelopment.com/index.php",
+                "kdssnyder.wordpress.com",
+                "zakdietzen.com"
+            };
+
         [TestMethod]
-        public void GetBlogPostsReturnsPosts()
+        public void GetRealBlogPostsReturnsPosts()
         {
             //Arrange
             var wordPressBlogReader = new WordPressBlogReader();
             var posts = new List<Post>();
 
-            // Real blog links
-            string[] blogLinks =
-                { "bracken.design",
-                   "jeffwarddevelopment.com/index.php",
-                   "kdssnyder.wordpress.com",
-                   "zakdietzen.com"
-            }; 
-
-            for (int i = 0; i < blogLinks.Length; i++)
+            for (int i = 0; i < _blogLinks.Length; i++)
             {
                 //Act
-                posts = wordPressBlogReader.GetBlogPosts(blogLinks[i]) as List<Post>;
+                posts = wordPressBlogReader.GetBlogPosts(_blogLinks[i]) as List<Post>;
 
                 //Assert
-                if (posts.Count == 0) Assert.Inconclusive("No posts found for " + blogLinks[i]);
-                Assert.IsTrue(posts[0].Link.Contains(blogLinks[i]));
+                if (posts.Count == 0) Assert.Inconclusive("No posts found for " + _blogLinks[i]);
+                Assert.IsTrue(posts[0].Link.Contains(_blogLinks[i]));
             }
+        }
+
+        [TestMethod]
+        public void VerifyRealBlogReturnsNotNull()
+        {
+            //Arrange
+            var wordPressBlogReader = new WordPressBlogReader();
+            BlogInfo blogInfo;
+
+            for (int i = 0; i < _blogLinks.Length; i++)
+            {
+                //Act
+                blogInfo = wordPressBlogReader.VerifyBlog(_blogLinks[i]);
+
+                //Assert
+                Assert.IsNotNull(blogInfo);
+            }
+        }
+
+        [TestMethod]
+        public void VerifyNonExistentBlogReturnsNull()
+        {
+            //Arrange
+            var wordPressBlogReader = new WordPressBlogReader();
+
+            //Act
+            BlogInfo blogInfo = wordPressBlogReader.VerifyBlog("someblogXXX");
+
+            //Assert
+            Assert.IsNull(blogInfo);
+
         }
     }
 }
